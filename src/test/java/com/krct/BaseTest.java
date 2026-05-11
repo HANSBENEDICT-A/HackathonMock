9b5ac87514c3b89b6e738c9a760d96bc0d54a081
HANSBENEDICT-A <benedict.ahb7@gmail.com>
2026-05-11 21:59:51 +0530

    modified base test by adding WebDriverManager

--- src/test/java/com/krct/BaseTest.java
+++ src/test/java/com/krct/BaseTest.java
@@ -1,5 +1,6 @@
 package com.krct;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
@@ -15,6 +16,7 @@ public class BaseTest {
     @BeforeMethod
     public void setup()
     {
+        WebDriverManager.chromedriver().setup();
         driver = new ChromeDriver();
         wait = new WebDriverWait(driver, Duration.ofSeconds(10));
     }

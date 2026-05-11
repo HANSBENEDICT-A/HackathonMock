a94b213e1a4b365794a5e2f4c1a09c6ed292dc30
HANSBENEDICT-A <benedict.ahb7@gmail.com>
2026-05-11 21:24:15 +0530

    AdminTest Done

---
+++ src/test/java/com/krct/Validation.java
@@ -0,0 +1,26 @@
+package com.krct;
+
+import com.krct.pages.LoginPage;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class Validation extends BaseTest{
+    @Test
+    public void emptyFieldValidationTest()
+    {
+        LoginPage loginPage = new LoginPage(driver, wait);
+        loginPage.NavigatePage();
+        loginPage.clickLoginButton();
+        Assert.assertEquals(loginPage.getRequiredMessage(), "Required");
+    }
+    @Test
+    public void invalidDateValidationTest()
+    {
+        Assert.assertTrue(true);
+    }
+    @Test
+    public void dropdownValidationTest()
+    {
+        Assert.assertTrue(true);
+    }
+}

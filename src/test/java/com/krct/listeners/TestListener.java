f0db748fc50a063c8a4086cd3ce05005a4761dd3
HANSBENEDICT-A <benedict.ahb7@gmail.com>
2026-05-11 22:23:15 +0530

    created TestListener

---
+++ src/test/java/com/krct/listeners/TestListener.java
@@ -0,0 +1,43 @@
+package com.krct.listeners;
+
+import com.krct.BaseTest;
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.io.FileHandler;
+import org.testng.ITestListener;
+import org.testng.ITestResult;
+
+import java.io.File;
+import java.io.IOException;
+
+public class TestListener extends BaseTest
+        implements ITestListener {
+
+    @Override
+    public void onTestFailure(
+            ITestResult result)
+    {
+        File screenshot =
+                ((TakesScreenshot) driver)
+                        .getScreenshotAs(
+                                OutputType.FILE
+                        );
+
+        try {
+
+            FileHandler.copy(
+                    screenshot,
+                    new File(
+                            "screenshots/"
+                                    + result.getName()
+                                    + ".png"
+                    )
+            );
+
+        } catch (IOException e) {
+
+            e.printStackTrace();
+        }
+    }
+}

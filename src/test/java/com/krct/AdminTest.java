2bcceb32b2f42b4b99b34400525662d806843a74
HANSBENEDICT-A <benedict.ahb7@gmail.com>
2026-05-11 21:12:19 +0530

    Admin Test almost done

---
+++ src/test/java/com/krct/AdminTest.java
@@ -0,0 +1,76 @@
+package com.krct;
+
+import com.krct.pages.AdminPage;
+import com.krct.pages.LoginPage;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import javax.swing.*;
+
+public class AdminTest extends BaseTest {
+        @Test
+        public void adminPageTest()
+        {
+            LoginPage loginPage = new LoginPage(driver, wait);
+
+            AdminPage adminPage = new AdminPage(driver, wait);
+            loginPage.NavigatePage();
+            loginPage.Login("Admin", "admin123");
+            adminPage.openAdminPage();
+            Assert.assertTrue(adminPage.isAdminPageOpened());
+    }
+    @Test
+    public void createUserTest()
+    {
+        LoginPage loginPage =
+                new LoginPage(driver, wait);
+
+        AdminPage adminPage =
+                new AdminPage(driver, wait);
+
+        loginPage.NavigatePage();
+
+        loginPage.Login("Admin", "admin123");
+
+        adminPage.createUser(
+                "testuser",
+                "Test@123"
+        );
+
+        Assert.assertTrue(true);
+    }
+    @Test
+    public void searchUserTest()
+    {
+        LoginPage loginPage =
+                new LoginPage(driver, wait);
+
+        AdminPage adminPage =
+                new AdminPage(driver, wait);
+
+        loginPage.NavigatePage();
+
+        loginPage.Login("Admin", "admin123");
+
+        adminPage.openAdminPage();
+
+        Assert.assertTrue(true);
+    }
+    @Test
+    public void deleteUserTest()
+    {
+        LoginPage loginPage =
+                new LoginPage(driver, wait);
+
+        AdminPage adminPage =
+                new AdminPage(driver, wait);
+
+        loginPage.NavigatePage();
+
+        loginPage.Login("Admin", "admin123");
+
+        adminPage.openAdminPage();
+
+        Assert.assertTrue(true);
+    }
+}
